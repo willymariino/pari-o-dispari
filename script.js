a08e06106fb7dfb336cac46ce6c0a92f8ab4ddf5
willymariino <wg.mariino@gmail.com>
2025-03-08 20:30:41 +0100

    added functions outside eventlistenere

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ const gameResult = document.getElementById("game-result")
 const winnerResult = document.getElementById("winner-result")
 // validazione dell'input dell'utente per il numero inserito
 
+// funzione per generare un numero casuale da 1 a 5
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1
+}
+
+// funzione per verificare se il numero è pari o dispari
+function verifyIfEvenOrOdd(numero) {
+    if (numero % 2 === 0) {
+        return "pari"
+    }
+    else {
+        return "dispari"
+    }
+}
+
 sendTask.addEventListener('click', function () {
 
     // convertire l'input dell'utente in numero
@@ -39,50 +55,50 @@ sendTask.addEventListener('click', function () {
 
     }
 
+
+
+    if (userGuess !== "pari" && userGuess !== "dispari") {
+        console.log("input non valido. ricarica la pagina e scegline un altro")
+        userGuessResult.innerText = "input non valido. ricarica la pagina e scegline un altro"
+    }
+
+    else {
+
+        // generazione random del numero del computer
+        let num1 = Math.round(Math.random() * 5) + 1;
+        console.log("numero generato dal computer", num1) // è uguale all'altro algoritmo, non so perchè questo funziona e stampa in console, mentre l'altro non stampava.
+        cpuNumberResult.innerText = "numero generato dal computer:" + " " + num1
+
+        // somma dei due numeri
+        let sum = parseInt(userNumber.value) + num1; { // prima avevo messo userNumber + num1 tra parentesi, rimane il dubbio su quando ci vogliono le parentesi e quando no
+            console.log(sum)
+            sumNumberResult.innerText = "la somma dei due numeri è:" + " " + sum.value
+        }
+
+        // calcolo se il risultato è pari o dispari
+        let result;
+        if (sum % 2 === 0) {
+            result = "pari";
+        }
+        else {
+            result = "dispari";
+        }
+        console.log("risultato", result);
+        gameResult.innertext = "risultato" + " " + result.value
+
+        // confronto tra la scelta dell'utente e il risultato della somma
+        if (userGuess === result) {
+            console.log("ha vinto l'utente");
+            winnerResult.innerText = "ha vinto l'utente"
+        }
+        else {
+            console.log("ha vinto il computer");
+            winnerResult.innerText = "ha vinto il computer"
+        }
+
+    }
+
 })
-
-if (userGuess !== "pari" && userGuess !== "dispari") {
-    console.log("input non valido. ricarica la pagina e scegline un altro")
-    userGuessResult.innerText = "input non valido. ricarica la pagina e scegline un altro"
-}
-
-else {
-
-    // generazione random del numero del computer
-    let num1 = Math.round(Math.random() * 5) + 1;
-    console.log("numero generato dal computer", num1) // è uguale all'altro algoritmo, non so perchè questo funziona e stampa in console, mentre l'altro non stampava.
-    cpuNumberResult.innerText = "numero generato dal computer:" + " " + num1
-
-    // somma dei due numeri
-    let sum = parseInt(userNumber.value) + num1; { // prima avevo messo userNumber + num1 tra parentesi, rimane il dubbio su quando ci vogliono le parentesi e quando no
-        console.log(sum)
-        sumNumberResult.innerText = "la somma dei due numeri è:" + " " + sum.value
-    }
-
-    // calcolo se il risultato è pari o dispari
-    let result;
-    if (sum % 2 === 0) {
-        result = "pari";
-    }
-    else {
-        result = "dispari";
-    }
-    console.log("risultato", result);
-    gameResult.innertext = "risultato" + " " + result.value
-
-    // confronto tra la scelta dell'utente e il risultato della somma
-    if (userGuess === result) {
-        console.log("ha vinto l'utente");
-        winnerResult.innerText = "ha vinto l'utente"
-    }
-    else {
-        console.log("ha vinto il computer");
-        winnerResult.innerText = "ha vinto il computer"
-    }
-
-}
-
-
 
 
 
